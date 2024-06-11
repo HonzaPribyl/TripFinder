0341cb1e808528b5deabcb2be882be24f921d5d2
Jan Přibyl <honza.pribyl95@gmail.com>
2024-06-11 19:49:54 +0200

    init

---
+++ src/main/java/com/example/tripfinder/TripfinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripfinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tripfinder/TripfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripfinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
