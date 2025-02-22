595a106448be0861eb8a02cd2d8af728248d7ba5
Jan Přibyl <honza.pribyl95@gmail.com>
2025-02-22 21:28:28 +0100

    Refactor JFuzzService

--- src/main/java/com/example/tripfinder/services/JFuzzService.java
+++ src/main/java/com/example/tripfinder/services/JFuzzService.java
@@ -7,133 +7,93 @@ import net.sourceforge.jFuzzyLogic.FunctionBlock;
 import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
 import net.sourceforge.jFuzzyLogic.rule.Variable;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class JFuzzService {
+
+    private static final String FIS_FILES_PATH = "src/main/resources/fcl/";
+
+    private static final Map<String, String> FILE_PATHS = Map.of(
+            "hotel_equipment", FIS_FILES_PATH + "hotel_equipment.fcl",
+            "hotel", FIS_FILES_PATH + "hotel.fcl",
+            "location", FIS_FILES_PATH + "location.fcl",
+            "hotel_and_location", FIS_FILES_PATH + "hotel_and_location.fcl",
+            "convenience", FIS_FILES_PATH + "convenience.fcl",
+            "journey", FIS_FILES_PATH + "journey.fcl",
+            "trip", FIS_FILES_PATH + "trip.fcl"
+    );
 
     @Nonnull
     public TripFuzzEvaluationDTO evaluate(
-            int familyFriendly,
-            int wifi,
-            int swimmingPool,
-            int stars,
-            int locPref,
-            int beachDist,
-            int airportPreference,
-            int foodPackagePreference,
-            float price,
-            boolean showCharts
-    ) {
-        String fileNameHotelEq = "src/main/resources/fcl/hotel_equipment.fcl";
-        String fileNameHotel = "src/main/resources/fcl/hotel.fcl";
-        String fileNameLocation = "src/main/resources/fcl/location.fcl";
-        String fileNameHotAndLoc = "src/main/resources/fcl/hotel_and_location.fcl";
-        String fileNameConvenience = "src/main/resources/fcl/convenience.fcl";
-        String fileNameJourney = "src/main/resources/fcl/journey.fcl";
-        String fileNameTrip = "src/main/resources/fcl/trip.fcl";
-        FIS fisEq = FIS.load(fileNameHotelEq,true);
-        FIS fisHot = FIS.load(fileNameHotel,true);
-        FIS fisLoc = FIS.load(fileNameLocation,true);
-        FIS fisHotAndLoc = FIS.load(fileNameHotAndLoc,true);
-        FIS fisConvenience = FIS.load(fileNameConvenience,true);
-        FIS fisJourney = FIS.load(fileNameJourney,true);
-        FIS fisTrip = FIS.load(fileNameTrip,true);
+            int familyFriendly, int wifi, int swimmingPool, int stars,
+            int locPref, int beachDist, int airportPreference, int foodPackagePreference,
+            float price, boolean showCharts) {
 
-        FunctionBlock functionBlockEq = fisEq.getFunctionBlock("hotel_equipment");
-        FunctionBlock functionBlockHotel = fisHot.getFunctionBlock("hotel");
-        FunctionBlock functionBlockLocation = fisLoc.getFunctionBlock("location");
-        FunctionBlock functionBlockHotelAndLocation = fisHotAndLoc.getFunctionBlock("hotel_and_location");
-        FunctionBlock functionBlockConvenience = fisConvenience.getFunctionBlock("convenience");
-        FunctionBlock functionJourney = fisJourney.getFunctionBlock("journey");
-        FunctionBlock functionTrip = fisTrip.getFunctionBlock("trip");
+        Map<String, FIS> fisMap = loadFuzzySystems();
+        Map<String, FunctionBlock> functionBlocks = getFunctionBlocks(fisMap);
 
         if (showCharts) {
-            JFuzzyChart.get().chart(functionBlockEq);
-            JFuzzyChart.get().chart(functionBlockHotel);
-            JFuzzyChart.get().chart(functionBlockLocation);
-            JFuzzyChart.get().chart(functionBlockHotelAndLocation);
-            JFuzzyChart.get().chart(functionBlockConvenience);
-            JFuzzyChart.get().chart(functionJourney);
-            JFuzzyChart.get().chart(functionTrip);
+            functionBlocks.values().forEach(JFuzzyChart.get()::chart);
         }
 
-        fisEq.setVariable("family_friendly", familyFriendly);
-        fisEq.setVariable("wifi", wifi);
-        fisEq.setVariable("swimming_pool", swimmingPool);
+        Variable equipment = evaluateSystem(fisMap.get("hotel_equipment"), functionBlocks.get("hotel_equipment"),
+                Map.of("family_friendly",(double) familyFriendly, "wifi", (double) wifi, "swimming_pool", (double) swimmingPool),
+                showCharts, "equipment");
 
-        fisEq.evaluate();
+        Variable hotel = evaluateSystem(fisMap.get("hotel"), functionBlocks.get("hotel"),
+                Map.of("stars", (double) stars, "equipment", equipment.getValue()),
+                showCharts, "hotel");
 
-        Variable equipment = functionBlockEq.getVariable("equipment");
-        if (showCharts) {
-            JFuzzyChart.get().chart(equipment, equipment.getDefuzzifier(), true);
-        }
+        Variable location = evaluateSystem(fisMap.get("location"), functionBlocks.get("location"),
+                Map.of("location_preference", (double) locPref, "beach_distance", (double) beachDist),
+                showCharts, "location_attributes");
 
-        fisHot.setVariable("stars", stars);
-        fisHot.setVariable("equipment", fisEq.getVariable("equipment").getValue());
+        Variable hotelAndLocation = evaluateSystem(fisMap.get("hotel_and_location"), functionBlocks.get("hotel_and_location"),
+                Map.of("hotel", hotel.getValue(), "location_attributes", location.getValue()),
+                showCharts, "hotel_and_location");
 
-        fisHot.evaluate();
+        Variable convenience = evaluateSystem(fisMap.get("convenience"), functionBlocks.get("convenience"),
+                Map.of("airport_preference", (double) airportPreference, "food_package_preference", (double) foodPackagePreference),
+                showCharts, "convenience");
 
-        Variable hotel = functionBlockHotel.getVariable("hotel");
-        if (showCharts) {
-            JFuzzyChart.get().chart(hotel, hotel.getDefuzzifier(), true);
-        }
+        Variable journey = evaluateSystem(fisMap.get("journey"), functionBlocks.get("journey"),
+                Map.of("convenience", convenience.getValue(), "price", (double) price),
+                showCharts, "journey");
 
-        fisLoc.setVariable("location_preference", locPref);
-        fisLoc.setVariable("beach_distance", beachDist);
-
-        fisLoc.evaluate();
-
-        Variable location = functionBlockLocation.getVariable("location_attributes");
-        if (showCharts) {
-            JFuzzyChart.get().chart(location, location.getDefuzzifier(), true);
-        }
-
-        fisHotAndLoc.setVariable("hotel", fisHot.getVariable("hotel").getValue());
-        fisHotAndLoc.setVariable("location_attributes", fisLoc.getVariable("location_attributes").getValue());
-
-        fisHotAndLoc.evaluate();
-
-        Variable hotelAndLocation = functionBlockHotelAndLocation.getVariable("hotel_and_location");
-        if (showCharts) {
-            JFuzzyChart.get().chart(hotelAndLocation, hotelAndLocation.getDefuzzifier(), true);
-        }
-
-        fisConvenience.setVariable("airport_preference", airportPreference);
-        fisConvenience.setVariable("food_package_preference", foodPackagePreference);
-
-        fisConvenience.evaluate();
-
-        Variable convenience = functionBlockConvenience.getVariable("convenience");
-        if (showCharts) {
-            JFuzzyChart.get().chart(convenience, convenience.getDefuzzifier(), true);
-        }
-
-        fisJourney.setVariable("convenience", fisConvenience.getVariable("convenience").getValue());
-        fisJourney.setVariable("price", price);
-
-        fisJourney.evaluate();
-
-        Variable journey = functionJourney.getVariable("journey");
-        if (showCharts) {
-            JFuzzyChart.get().chart(journey, journey.getDefuzzifier(), true);
-        }
-
-        fisTrip.setVariable("journey", fisJourney.getVariable("journey").getValue());
-        fisTrip.setVariable("hotel_and_location", fisHotAndLoc.getVariable("hotel_and_location").getValue());
-
-        fisTrip.evaluate();
-
-        Variable trip = functionTrip.getVariable("trip");
-        if (showCharts) {
-            JFuzzyChart.get().chart(trip, trip.getDefuzzifier(), true);
-        }
+        Variable trip = evaluateSystem(fisMap.get("trip"), functionBlocks.get("trip"),
+                Map.of("journey", journey.getValue(), "hotel_and_location", hotelAndLocation.getValue()),
+                showCharts, "trip");
 
         return new TripFuzzEvaluationDTO(
-                equipment.getValue(),
-                hotel.getValue(),
-                location.getValue(),
-                hotelAndLocation.getValue(),
-                convenience.getValue(),
-                journey.getValue(),
-                trip.getValue()
+                equipment.getValue(), hotel.getValue(), location.getValue(),
+                hotelAndLocation.getValue(), convenience.getValue(),
+                journey.getValue(), trip.getValue()
         );
+    }
+
+    private Map<String, FIS> loadFuzzySystems() {
+        return FILE_PATHS.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> FIS.load(entry.getValue(), true)));
+    }
+
+    private Map<String, FunctionBlock> getFunctionBlocks(Map<String, FIS> fisMap) {
+        return fisMap.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().getFunctionBlock(entry.getKey())));
+    }
+
+    private Variable evaluateSystem(
+            FIS fis,
+            FunctionBlock functionBlock,
+            Map<String, Double> inputs,
+            boolean showCharts,
+            String variableName) {
+        inputs.forEach(fis::setVariable);
+        fis.evaluate();
+        Variable result = functionBlock.getVariable(variableName);
+        if (showCharts) {
+            JFuzzyChart.get().chart(result, result.getDefuzzifier(), true);
+        }
+        return result;
     }
 }
